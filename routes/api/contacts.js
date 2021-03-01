885cd6735d02aee2cc56f84fdf99cf4030f5052b
Vitaliy Solomka <dkvvs@ukr.net>
2021-03-02 01:08:05 +0200

    add 03-mongodb

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,115 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const Contacts = require("../../model/index");
 const validate = require("./validation");
+const contactsController = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await Contacts.listContacts();
-    return res.json({
-      status: "success",
-      code: 200,
-      data: {
-        contacts,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
-});
+router
+  .get("/", contactsController.listContacts)
+  .post("/", validate.addContact, contactsController.addContact);
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const contact = await Contacts.getContactById(req.params.id);
-    if (contact) {
-      return res.json({
-        status: "success",
-        code: 200,
-        data: {
-          contact,
-        },
-      });
-    } else {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        message: "Not found",
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.post("/", validate.addContact, async (req, res, next) => {
-  try {
-    const contact = await Contacts.addContact(req.body);
-    return res.status(201).json({
-      status: "success",
-      code: 201,
-      data: {
-        contact,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const contact = await Contacts.getContactById(req.params.id);
-    if (contact) {
-      await Contacts.removeContact(req.params.id);
-      return res.json({
-        status: "success",
-        code: 200,
-        message: "Contact deleted",
-      });
-    } else {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        message: "Not Found",
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.patch("/:id", validate.updateContact, async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const body = req.body;
-    if (!body.name && !body.email && !body.phone) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: "Missing fields",
-      });
-    }
-    const contactId = await Contacts.getContactById(id);
-    if (contactId) {
-      await Contacts.updateContact(id, body);
-      const contact = await Contacts.getContactById(id);
-      return res.json({
-        status: "success",
-        code: 200,
-        data: {
-          contact,
-        },
-      });
-    } else {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        message: "Not found",
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
+router
+  .get("/:id", contactsController.getContactById)
+  .delete("/:id", contactsController.removeContact)
+  .patch("/:id", validate.updateContact, contactsController.updateContact);
 
 module.exports = router;
